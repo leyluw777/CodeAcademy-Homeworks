6450a615fb6b56c3cad6dfc96a8ed0269c2a09c2
Leila <leila.imanova.7@gmail.com>
2023-05-03 23:32:50 +0400

    enums task added

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace Enums
+{
+
+    public enum Status : byte
+    {
+     
+
+
+        [Description("Aktif Kullanıcıları Listeler")]
+        Active ,
+
+
+        [Description("Pasif Kullanıcıları Listeler")]
+        Passive,
+
+
+        [Description("Silinmiş Kullanıcıları Listeler")]
+        Deleted,
+
+
+        [Description("Kaydını Dondurmuş Kullanıcıları Listeler")]
+        Holiday
+    }
+
+   
+    public static class Program
+    {
+        public static string GetDescription(this Enum value)
+        {
+            var fieldInfo = value.GetType().GetField(value.ToString());
+            var attribute = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+
+            return attribute != null && attribute.Length > 0 ? attribute[0].Description : value.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (Status status in Enum.GetValues(typeof(Status)))
+            {
+                Console.WriteLine("{0} - {1}", status, status.GetDescription());
+            }
+
+    
+        }
+    }
+}
