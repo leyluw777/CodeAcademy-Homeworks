3ac2e641fa168603b3ad00f0af9b432273004bc4
Leila <leila.imanova.7@gmail.com>
2023-05-14 17:50:24 +0400

    vcard task added

---
+++ VCard/VCard/Models/PersonData.cs
@@ -0,0 +1,201 @@
+﻿//using HttpClientExtensionMethods;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Reflection;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace VCard.Models
+//{
+//    class Result 
+//    {
+//        public string Gender { get; set; }
+//        public string Email { get; set; }
+//        public string Phone { get; set; }
+//        public string Cell { get; set; }
+//        public string Nat { get; set; }
+//        public Name Name { get; set; }
+//        public Dob Dob { get; set; }
+//    }
+
+
+//    class Name
+//    {
+//        private string _title;
+//        private string _first;
+//        private string _last;
+
+//        public string Title {
+//            get => _title;
+//            set
+//            {
+//                _title = value;
+//            }
+//        }
+//        public string First {
+//            get => _first;
+//            set
+//            {
+//                _first = value;
+//            }
+//        }
+//        public string Last {  get => _last; set
+//            {
+//                _last = value;
+//            }
+//        }
+
+//        public string FullName => $"{_first} {_last}";
+//    }
+
+//    class Info
+//    {
+//        public string Seed { get; set; }
+//        public int Results { get; set; }
+//        public int Page { get; set; }
+//        public string Version { get; set; }
+//    }
+
+
+//}
+
+
+namespace VCard.Models
+{
+    public class Result
+    {
+        public string Gender { get; set; }
+        public Name Name { get; set; }
+        public Location Location { get; set; }
+        public string Email { get; set; }
+        public Login Login { get; set; }
+        public Dob Dob { get; set; }
+        public Registered Registered { get; set; }
+        public string Phone { get; set; }
+        public string Cell { get; set; }
+        public Id Id { get; set; }
+        public Picture Picture { get; set; }
+        public string Nat { get; set; }
+        
+    }
+
+    public class Name
+    {
+        private string _title;
+        private string _first;
+        private string _last;
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+            }
+        }
+        public string First
+        {
+            get => _first;
+            set
+            {
+                _first = value;
+            }
+        }
+        public string Last
+        {
+            get => _last; set
+            {
+                _last = value;
+            }
+        }
+
+        public string FullName => $"{_first} {_last}";
+    }
+
+    public class Location
+    {
+        public Street Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public int Postcode { get; set; }
+        public Coordinates Coordinates { get; set; }
+        public Timezone Timezone { get; set; }
+    }
+
+    public class Street
+    {
+        public int _number;
+        public string _name;
+        public int Number { get => _number; set
+            {
+                _number = value;
+            }
+                
+                }
+        public string Name { get => _name; set
+            {
+                _name = value;
+            }
+        }
+
+        public string StreetName => $"{_name} {_number}";
+    }
+
+    public class Coordinates
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+
+    public class Timezone
+    {
+        public string Offset { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Login
+    {
+        public string Uuid { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+        public string Md5 { get; set; }
+        public string Sha1 { get; set; }
+        public string Sha256 { get; set; }
+    }
+
+    public class Dob
+    {
+        public DateTime Date { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Registered
+    {
+        public DateTime Date { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Id
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class Picture
+    {
+        public string Large { get; set; }
+        public string Medium { get; set; }
+        public string Thumbnail { get; set; }
+    }
+
+    public class Info
+    {
+        public string Seed { get; set; }
+        public int Results { get; set; }
+        public int Page { get; set; }
+        public string Version { get; set; }
+    }
+}
---
+++ VCard/VCard/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using VCard.Models;
+using System.Text.Json;
+using System;
+//using Newtonsoft.Json;
+
+namespace HttpClientExtensionMethods
+{
+    using static VCard.Models.Result;
+    class Response
+    {
+        public List<Result> Results { get; set; }
+        public Info Info { get; set; }
+    }
+
+
+    public class Program
+    {
+   
+
+      
+        public static async Task Main() {
+
+            HttpClient client = new HttpClient();
+            Response response = await client.GetFromJsonAsync<Response>("https://randomuser.me/api?results=1");
+            Result result = response.Results.FirstOrDefault();
+
+
+            string vcard = $@"
+BEGIN:VCARD
+VERSION:3.0
+FN;CHARSET=UTF-8:{result.Name.FullName}
+N;CHARSET=UTF-8:{result.Name.Last};{result.Dob.Date.ToString("yyyy-MM-dd")};;;
+BDAY:{result.Dob.Date.ToString("MM-dd")}
+EMAIL;CHARSET=UTF-8;type=HOME,INTERNET:{result.Email}
+TEL;TYPE=HOME,VOICE:{result.Phone}
+ADR;CHARSET=UTF-8;TYPE=HOME:;;{result.Location.Street.StreetName};{result.Location.City};;{result.Location.Postcode};{result.Location.Country}
+REV:{result.Registered.Date.ToString("yyyy-MM-ddTHH:mm:ss.fffZ")}
+END:VCARD 
+";
+
+            Console.WriteLine(vcard);
+
+            string[] vcardArr = vcard.Split(' ');
+            Console.WriteLine(vcardArr.Length);
+            string pathName = @"C:\Users\Hp\source\repos\VCard\VCard\Vcard.vcf";
+            FileStream fs = File.Create(pathName);
+            fs.Close();
+            File.WriteAllLines(pathName, vcardArr);
+        }
+
+
+      
+   
+
+    }
+}
